c6abc249545698fd2466e140ab5f765b116b7de8
Aniket Pal <aniketpal621@gmail.com>
2024-06-28 20:06:41 +0530

    deleted

--- src/app/test.tsx
+++
@@ -1,1 +0,0 @@
-<a href="https://www.freepik.com/free-vector/tiny-people-stock-traders-laptop-with-graph-chart-buy-sell-shares-stock-market-index-stockbroking-company-stock-exchange-data-concept_10783788.htm#query=buy%20sell%20trading&position=0&from_view=keyword&track=ais_user&uuid=c851363f-bd5c-438c-82e8-77db3122d97d">Image by vectorjuice on Freepik</a>
