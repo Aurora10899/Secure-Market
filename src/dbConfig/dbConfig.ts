402107be656896708882395938bb09a0d05b7b0a
Aniket Pal <aniketpal621@gmail.com>
2024-06-28 19:51:59 +0530

    Updated some comments

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
 import events from 'events';
 
-// Increase the maximum number of listeners globally
+
 events.EventEmitter.defaultMaxListeners = 30; // Adjust as necessary
 
-let isConnected = false; // Track the connection state
+let isConnected = false; 
 
 export async function connect() {
     if (isConnected) {
-        return; // If already connected, return early
+        return; 
     }
 
     try {
@@ -18,39 +18,17 @@ export async function connect() {
 
         connection.once('connected', () => {
             console.log('MongoDB connected successfully');
-            isConnected = true; // Set the connection state
+            isConnected = true; 
         });
 
         connection.once('error', (err) => {
             console.error('MongoDB connection failed:', err);
-            process.exit(); // Exit with an error code
+            process.exit(); 
         });
 
     } catch (error) {
         console.error('Something went wrong:', error);
-        process.exit(); // Exit with an error code
+        process.exit(); 
     }
 }
 
-// import mongoose from 'mongoose';
-
-// export async function connect() {
-//     try {
-//         mongoose.connect(process.env.MONGO_URI!);
-//         const connection = mongoose.connection;
-
-//         connection.on('connected', () => {
-//             console.log('MongoDB connected Successfully');
-//         })
-
-//         connection.on('error', (err) => {
-//             console.log('MongoDB connection failed' + err);
-//             process.exit();
-//         })
-            
-            
-//     } catch (error) {
-//         console.log('Something went wrong');
-//         console.log(error);        
-//     }
-// }
